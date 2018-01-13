e81736d55aef9a2d6dae8fa211c393b0e970db01
Steven Abadie <steven@anothernewthing.com>
2018-01-13 12:15:01 -0700

    Add initial BeautifulSoup testing and for loop to filter quotes

--- getJacksQuotes.py
+++ getJacksQuotes.py
@@ -1,11 +1,23 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-#Fight Club quote source
+# Quote source page from rotten tomatoes
 url = "https://www.rottentomatoes.com/m/fight_club/quotes/"
 
-#Download page for parsing
+# Request page for parsing
 page = requests.get(url)
 parsedPage = bs(page.text, 'html.parser')
 
-print(parsedPage)
+'''
+Early testing of the BeautifulSoup package, will remove later
+
+#print(parsedPage)
+#quoteActor = parsedPage.find('span', class_="quote_actor")
+#print(quoteActor.next_element.next_element.next_element.next_element)
+'''
+
+for quote in parsedPage.find_all('span', class_="quote_actor"):
+    line = quote.next_element.next_element.next_element.next_element
+    if len(line) <= 500:
+        print(len(line))
+        print(line + "\n")
