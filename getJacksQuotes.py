ffbf6ad8e6431acfa9d5c8b07218632a40f4d6d7
Steven Abadie <steven@anothernewthing.com>
2018-01-13 16:45:28 -0700

    Change loop to append quote to list and then save to json file
    Added quoteList list variable that quotes are now appended to in the quote loop.The list is then saved to quotes.json. Added quotes.json to .gitignore. Currently there is no mechanism to catch duplicate quotes. This will be added later.
    
    Close #2

--- getJacksQuotes.py
+++ getJacksQuotes.py
@@ -1,23 +1,34 @@
 import requests
+import json
 from bs4 import BeautifulSoup as bs
 
 # Quote source page from rotten tomatoes
 url = "https://www.rottentomatoes.com/m/fight_club/quotes/"
 
-# Request page for parsing
+# Request page from url, parse for use with bs, and set to parsedPage
 page = requests.get(url)
 parsedPage = bs(page.text, 'html.parser')
 
-'''
-Early testing of the BeautifulSoup package, will remove later
+# Create quoteList and load quotes.json into it.
+# quotes.json is created if it does not already exist
+quoteList = []
+try:
+    with open('quotes.json', 'r') as quoteFile:
+        quoteList = json.load(quoteFile)
+except Exception:
+    with open('quotes.json', 'w') as quoteFile:
+        json.dump(quoteList, quoteFile)
 
-#print(parsedPage)
-#quoteActor = parsedPage.find('span', class_="quote_actor")
-#print(quoteActor.next_element.next_element.next_element.next_element)
-'''
-
+# Find all quotes in parsedPage and append to quoteList
 for quote in parsedPage.find_all('span', class_="quote_actor"):
     line = quote.next_element.next_element.next_element.next_element
     if len(line) <= 500:
-        print(len(line))
-        print(line + "\n")
+        quoteList.append(line)
+
+# Sample of loading quotes.json into quoteList and then printing the quotes
+# This is only here for testing and will be removed later
+with open('quotes.json', 'w') as quoteFile:
+    json.dump(quoteList, quoteFile)
+
+for quote in quoteList:
+    print(str(quote) + "\n")
