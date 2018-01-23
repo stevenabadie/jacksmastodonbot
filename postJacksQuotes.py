d667e51c7606e6606683a2df07bbc4c6720165f6
Steven Abadie <steven@anothernewthing.com>
2018-01-22 17:01:17 -0700

    Further define postJacksQuotes to post next quote from quotes.json
    This adds functionality to track which quote to post next by tracking the next quote number in nextQuote.json. quotes.json is opened and then used to toot the next quote to the account defined with createJacksApp

--- postJacksQuotes.py
+++ postJacksQuotes.py
@@ -1,11 +1,39 @@
+import json
 from mastodon import Mastodon
 from createJacksApp import botName, mastodonServerUrl
 
-# Create API instance
+# Create Mastodon API instance
 mastodon = Mastodon(
     client_id = botName + '_clientcred.secret',
     access_token = botName + '_usercred.secret',
-    api_base_url = mastodonServerUrl
+    api_base_url = mastodonServerUrl,
+    request_timeout = 20
 )
 
-mastodon.toot('This is a test tootoot.')
+# Create nextQuote and try to load nextQuote.json into it. If nextQuote.json
+# does not exist, create the file and assign 0 to nextQuote. nextQuote is used
+# to track which quote in the list is next to post.
+nextQuote = None
+try:
+    with open('nextQuote.json', 'r') as nextQuoteFile:
+        nextQuote = int(json.load(nextQuoteFile))
+except Exception:
+    with open('nextQuote.json', 'w') as nextQuoteFile:
+        json.dump(nextQuote, nextQuoteFile)
+    nextQuote = 0
+
+# Load quotes.json into quoteList
+with open('quotes.json', 'r') as quoteFile:
+    quoteList = json.load(quoteFile)
+
+# Post quote if there are unposted quotes. Otherwise, raise an exception.
+if nextQuote <= len(quoteList):
+    mastodon.toot(quoteList[nextQuote])
+else:
+    raise StopIteration("There are no quotes left to post")
+
+# Increase nextQuote by one and dump to nextQuote.json. The next time the
+# script is run the next quote in the list will be used.
+nextQuote += 1
+with open('nextQuote.json', 'w') as nextQuoteFile:
+    json.dump(nextQuote, nextQuoteFile)
